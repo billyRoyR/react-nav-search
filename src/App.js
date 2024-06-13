6ab4efa7fc4bfba9fc725a5cc76df18df135cf67
billyRoyR <156974090+billyRoyR@users.noreply.github.com>
2024-06-14 00:11:50 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Navbar from './components/navbar/Navbar'
 
-function App() {
+const App = () => {
+
+  const current_theme = localStorage.getItem('current_theme');
+
+  const [theme, setTheme] = useState(current_theme ? current_theme : 'light');
+
+  useEffect(() => {
+    localStorage.setItem('current_theme', theme);
+  },[theme])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className={`container ${theme}`}>
+        <Navbar theme={theme} setTheme={setTheme}/>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
